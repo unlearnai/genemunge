733146e6ff8a761ebebb3f1bf71643846cb82d7d
Charles Fisher <charleskennethfisher@gmail.com>
2017-08-01 15:16:13 -0700

    downloading script

---
+++ genemunge/data/downloads.py
@@ -0,0 +1,34 @@
+import urllib, os, sys
+
+FILEPATH = os.path.dirname(os.path.abspath(__file__))
+LINK_TO_GO = "http://purl.obolibrary.org/obo/go/go-basic.obo"
+GONAME = "go-basic.obo"
+LINK_TO_HGNC = "ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt"
+HGNCNAME = "hgnc_complete_set.txt"
+
+def download_progress_indicator(count, blockSize, totalSize):
+      percent = int(count*blockSize*100/totalSize)
+      sys.stdout.write("\rProgress... %d%%" % percent)
+      sys.stdout.flush()
+
+def download_go(force=False):
+    output_file = os.path.join(FILEPATH, GONAME)
+    output_exists = os.path.exists(output_file)
+    if not output_exists or force:
+        print("downloading {}".format(GONAME))
+        urllib.request.urlretrieve(LINK_TO_GO, os.path.join(FILEPATH, GONAME),
+                               reporthook=download_progress_indicator)
+        print("done")
+
+def download_hgnc(force=False):
+    output_file = os.path.join(FILEPATH, HGNCNAME)
+    output_exists = os.path.exists(output_file)
+    if not output_exists or force:
+        print("\ndownloading {}".format(HGNCNAME))
+        urllib.request.urlretrieve(LINK_TO_HGNC, os.path.join(FILEPATH, HGNCNAME),
+                               reporthook=download_progress_indicator)
+        print("done")
+
+if __name__ == "__main__":
+    download_go()
+    download_hgnc()
