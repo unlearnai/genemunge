f518acc2cf22810d03ceb5987e89d2240b371231
Charles Fisher <charleskennethfisher@gmail.com>
2017-08-23 09:56:22 -0700

    download annotations

--- genemunge/data/parse_go.py
+++ genemunge/data/parse_go.py
@@ -3,7 +3,11 @@ import os, re
 filepath = os.path.dirname(os.path.abspath(__file__))
 gofile = os.path.join(filepath, "go-basic.obo")
 
-patter ='id:'
+go_id = re.compile('id: GO:[0-9]{7}')
+is_a = re.compile('is_a: GO:[0-9]{7}')
+part_of = re.compile('part_of GO:[0-9]{7}')
+name = {}
+
 
 def has_pattern(string, pattern):
     x = re.match(pattern, string)
@@ -12,6 +16,9 @@ def has_pattern(string, pattern):
     else:
         return True
 
+def is_parent(string):
+    return (("is_a" in string.lower()) or ("part_of" in string.lower()))
+
 def split_entry(entry):
     return [line.strip().split(':', 1) for line in entry]
 
@@ -19,7 +26,8 @@ def parse_entry(entry):
     pass
 
 # id: name, namespace, def, parents, children
-# connections (parent/child): 'is_a' or 'part_of' without 'not'
+# connections (parent/child): 'is_a' or 'part_of'
+# (seems like NOT annotations have been removed?)
 # ignore if 'is_obsolete: true'
 
 # read in the ontology file
