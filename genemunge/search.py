d9711bce65b48db849cbbad68dd69f6833096888
Charles Fisher <charleskennethfisher@gmail.com>
2017-08-31 15:48:04 -0700

    select namespace

--- genemunge/search.py
+++ genemunge/search.py
@@ -3,7 +3,6 @@ import os, json, itertools
 FILEPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 FILENAME = os.path.join(FILEPATH, 'go.json')
 
-
 class Searcher(object):
 
     def __init__(self):
@@ -17,6 +16,7 @@ class Searcher(object):
             relatives = []
 
         new_relatives = self.go[term]['children']
+        print(new_relatives)
 
         while True:
             update = list(set(relatives).union(new_relatives))
@@ -27,3 +27,7 @@ class Searcher(object):
             new_relatives = itertools.chain.from_iterable([self.go[t]['children'] for t in new_relatives])
 
         return relatives
+
+    def select_namespace(self, namespace):
+        return [term for term in self.go if self.go[term]['namespace'] == namespace]
+
