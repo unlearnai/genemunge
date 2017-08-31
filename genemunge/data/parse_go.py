6061bb36c6258aec3da4e433d6b5827d9d7ccba3
Charles Fisher <charleskennethfisher@gmail.com>
2017-08-31 15:43:36 -0700

    traverse children

--- genemunge/data/parse_go.py
+++ genemunge/data/parse_go.py
@@ -184,8 +184,16 @@ def make_godict(gofile, force=False):
                         pass
 
     # remove any empty go categories
-    nonempty_godict = {term: godict[term] for term in godict
-    if len(set(itertools.chain.from_iterable(godict[term]['genes'].values()))) > 0}
+    empty_terms = [term for term in godict if
+    len(set(itertools.chain.from_iterable(godict[term]['genes'].values()))) == 0]
+    nonempty_godict = {term: godict[term] for term in godict if term not in empty_terms}
+
+    # remove the empty go categories from parents and children lists
+    for term in nonempty_godict:
+        nonempty_godict[term]['parents'] = \
+        list(set(nonempty_godict[term]['parents']).difference(empty_terms))
+        nonempty_godict[term]['children'] = \
+        list(set(nonempty_godict[term]['children']).difference(empty_terms))
 
     # write to the file
     with open(OUTPUTFILE, "w") as outfile:
--- genemunge/search.py
+++ genemunge/search.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, itertools
 
 FILEPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 FILENAME = os.path.join(FILEPATH, 'go.json')
@@ -9,3 +9,21 @@ class Searcher(object):
     def __init__(self):
         with open(FILENAME, 'r') as infile:
             self.go = json.load(infile)
+
+    def traverse(self, term, inclusive=True):
+        if inclusive:
+            relatives = [term]
+        else:
+            relatives = []
+
+        new_relatives = self.go[term]['children']
+
+        while True:
+            update = list(set(relatives).union(new_relatives))
+            if len(relatives) == len(update):
+                break
+            else:
+                relatives = update
+            new_relatives = itertools.chain.from_iterable([self.go[t]['children'] for t in new_relatives])
+
+        return relatives
