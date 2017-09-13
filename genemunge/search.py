95d2b0c0516bc8505a3e0959ab64d10f67563907
Charles Fisher <charleskennethfisher@gmail.com>
2017-09-13 16:09:09 -0700

    add function to get proteins from go terms

--- genemunge/search.py
+++ genemunge/search.py
@@ -40,4 +40,18 @@ class Searcher(object):
         else:
             return list(set(itertools.chain.from_iterable([self.traverse(t) for t in exact_terms])))
 
+    def _get_proteins_from_term(self, term, evidence_codes):
+        proteins = []
+        for code in self.go[term]["genes"]:
+            if evidence_codes is None or code in evidence_codes:
+                proteins += self.go[term]["genes"][code]
+        return proteins
+
+    def get_proteins(self, terms, evidence_codes = None):
+        if evidence_codes is not None:
+            assert type(evidence_codes) == list, \
+            "evidence_codes must be None or a list of GO evidence codes"
+        all_proteins = [self._get_proteins_from_term(t, evidence_codes) for t in terms]
+        return list(set().union(*all_proteins))
+
 
