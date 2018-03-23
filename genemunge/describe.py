bc5021b61586edd6780fb52d8d73f8dbdb48101e
Charles Fisher <charleskennethfisher@gmail.com>
2018-03-23 13:37:43 -0700

    get tissue stats for a gene

--- genemunge/describe.py
+++ genemunge/describe.py
@@ -1,14 +1,17 @@
 import os
 import pandas
-from cytoolz import identity
 
 from . import convert
 from . import search
 
+
 datapath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 gtexpath = os.path.join(datapath, 'gtex')
 
+
 class Describer(object):
+
+    __stats__ = ['mean', 'median', 'std', 'lower_quartile', 'upper_quartile']
 
     def __init__(self, identifier='symbol'):
         """
@@ -29,13 +32,21 @@ class Describer(object):
         self.get_name = convert.IDConverter('ensembl_gene_id', 'name').convert
         self.get_symbol = convert.IDConverter('ensembl_gene_id', 'symbol').convert
         self.searcher = search.Searcher()
+        self.tissue_stats = pandas.HDFStore(os.path.join(gtexpath, 'tissue_stats.h5'))
+
+    def close(self):
+        """
+
+        """
+        self.tissue_stats.close()
 
     def get_tissue_expression(self, identifier):
         """
 
         """
-        pass
-
+        i = self.get_ensembl(identifier)
+        return pandas.concat(
+                {k: self.tissue_stats[k].loc[i] for k in self.__stats__}, axis=1)
 
     def plot_tissue_expression(self, idenifier):
         """
